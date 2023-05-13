814c868627cc5de7df27e6615b1b076962b09097
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-05-13 11:55:13 -0700

    fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,7 @@
-# Ensure the class-wp-locale.php file has the correct permissions
-file { '/var/www/html/wp-includes/class-wp-locale.php':
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0644',
-  content => file('/var/www/html/wp-includes/class-wp-locale.phpp'),
-}
-
-# Restart the Apache service if the class-wp-locale.php file has been updated
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/var/www/html/wp-includes/class-wp-locale.php'],
+# Puppet code to fix Apache 500 error caused by missing or incorrect permissions for class-wp-locale.php file
+# Attach strace to the Apache process and run curl to identify the issue
+# This Puppet code ensures the class-wp-locale.php file is present with correct permissions
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
+ensure => present,
+source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
