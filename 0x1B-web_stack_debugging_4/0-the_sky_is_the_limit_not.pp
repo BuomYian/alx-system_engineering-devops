89fa44b284ae81bd2e2a2ad8d2011afffafc5be0
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-05-16 13:17:41 -0700

    puppet-lint debug

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fix and allow unlimited traffic/requests
+# HTTP request to a web server
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
